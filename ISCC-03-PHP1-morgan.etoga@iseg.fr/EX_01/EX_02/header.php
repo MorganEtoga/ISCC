08353f93976bb153c703f99289e7e645224411a9
albantsr <albanteissier191@yahoo.fr>
2023-04-03 14:32:07 +0200

    Ajout des includes

--- ISCC-03-PHP1-morgan.etoga@iseg.fr/EX_01/EX_02/header.php
+++ ISCC-03-PHP1-morgan.etoga@iseg.fr/EX_01/EX_02/header.php
@@ -1,10 +1,11 @@
-<header.php>
-  <body>
-    <h2>Space Proteine</h2><img src="logo.png">
-    <br><nav>
-      <ul>
-        <li><a href="accueil.html">Accueil</a></li>
-        <li><a href="connexion.html">&Agrave; propos</a></li>
-        <li><a href="apropos.html">Services</a></li>
-      </ul>
-    </nav></header.php>
+<header>
+  <h2>Space Proteine</h2><img src="logo.png">
+  <br>
+  <nav>
+    <ul>
+      <li><a href="accueil.php">Accueil</a></li>
+      <li><a href="connexion.php">&Agrave; propos</a></li>
+      <li><a href="apropos.php">Services</a></li>
+    </ul>
+  </nav>
+</header>
